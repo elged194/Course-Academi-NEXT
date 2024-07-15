5608215da969ff8fa8ee5b559084e18c26a14762
Hussein Elged <elged194@gmail.com>
2024-07-15 14:35:17 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "res.cloudinary.com",
-          port: "",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: "images.unsplash.com",
-          port: "",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: "plus.unsplash.com",
-          port: "",
-          pathname: "/**",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  // https://images.unsplash.com
-  
+  images: {
+    remotePatterns: [
+      // {
+      //   protocol: "https",
+      //   hostname: "res.cloudinary.com",
+      //   port: "",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "images.unsplash.com",
+      //   port: "",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "plus.unsplash.com",
+      //   port: "",
+      //   pathname: "/**",
+      // },
+
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
